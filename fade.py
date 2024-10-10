e1002a988d1e20ffe84d8292a7e75ae7ec7cf5c7
potatonvamp <2026cambo.oly@fcboe.org>
2024-10-10 13:29:24 -0400

    Update fade.py
    Made a fade in function, made it so it fades in and fades out with random colors, and added a parameter that allows you to change the time more easily.

--- fade.py
+++ fade.py
@@ -5,26 +5,49 @@ import random
 
 np = NeoPixel(board.D2, 30, auto_write=False, brightness=0.3)
 
+red = (255, 0, 0)
+blue = (0, 0, 255)
+green = (0, 255, 37)
+purple = (255, 0, 255)
+yellow = (255, 255, 0)
+cyan = (0, 255, 255)
+orange = (255, 64, 0)
+color_list = [red, green, blue, purple, yellow, cyan, orange]
 
-def fade_out(color):
+
+def fade_out(color, fade_time):
     red_ratio = color[0] / 50
     red_org = color[0]
+    green_ratio = color[1] / 50
+    green_org = color[1]
+    blue_ratio = color[2] / 50
+    blue_org = color[2]
     for i in range(1, 51):
-        red = red_org - i * red_ratio
-        np.fill((red, 0, 0))
+        r = red_org - i * red_ratio
+        g = green_org - i * green_ratio
+        b = blue_org - i * blue_ratio
+        np.fill((r, g, b))
         np.show()
-        time.sleep(0.03)
+        time.sleep(fade_time)
 
-def fade_in(color2):
+def fade_in(color2, fade_time2):
     red_ratio = color2[0] / 50
     red_org = color2[0]
+    green_ratio = color2[1] / 50
+    green_org = color2[1]
+    blue_ratio = color2[2] / 50
+    blue_org = color2[2]
     for i in range(1, 51):
-        red = red_org + i * red_ratio
-        np.fill((red, 0, 0))
+        r = red_org + i * red_ratio
+        g = green_org + i * green_ratio
+        b = blue_org + i * blue_ratio
+        np.fill((r, g, b))
         np.show()
-        time.sleep(0.03)
+        time.sleep(fade_time2)
 
 while True:
-    fade_out((255,0,0))
-    fade_in((255,0,0))
+    nice = random.choice(color_list)
+    tiempo = 0.02
+    fade_in(nice, tiempo)
+    fade_out(nice, tiempo)
 
