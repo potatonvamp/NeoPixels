3bddf66e2126213e6fb7c8c7f3e7643f2e906799
potatonvamp <2026cambo.oly@fcboe.org>
2024-10-29 13:46:34 -0400

    Update halloween.py
    Finished the while true and started the docstrings

--- halloween.py
+++ halloween.py
@@ -21,6 +21,17 @@ halloween_list = [orange, purple, green]
 
 
 def sparkle(bg, fg, delay, spark):
+    """
+    
+    
+    Args:
+        bg(tuple): the color used in the background of the NeoPixels
+        fg(tuple): the color that blinks around in the NeoPixels
+        delay(float): the length the function sleeps for.
+        Spark(int): the length of time it wil sparkle for.
+    
+    Return: none
+    """
     for i in range(spark):
         one = random.randint(0,29)
         two = random.randint(0,29)
@@ -34,12 +45,24 @@ def sparkle(bg, fg, delay, spark):
 
 
 def fire(flicker = 15):
+    """
+    
+    Args:
+    
+    Return: none
+    """
     for i in range(flicker):
         sparkle(orange , purple, 0.1, 0.1)
         sparkle(orange, green, 0.2, 0.1)
         sparkle(orange, black, 0.02, 0.1)
 
 def lightning(bg, flash):
+    """
+    
+    Args:
+    
+    Return: none
+    """
     yes = random.randint(1,2)
     for i in range(yes):
         no = random.randint(1, 3)
@@ -52,6 +75,12 @@ def lightning(bg, flash):
         time.sleep(no)
         
 def fade_out(color, fade_time, number = 50):
+    """
+    
+    Args:
+    
+    Return: none
+    """
     red_ratio = color[0] / number
     red_org = color[0]
     green_ratio = color[1] / number
@@ -67,6 +96,12 @@ def fade_out(color, fade_time, number = 50):
         time.sleep(fade_time)
 
 def fade_in(color2, fade_time2, number2 = 50):
+    """
+    
+    Args:
+    
+    Return: none
+    """
     red_ratio = color2[0] / number2
     red_org = color2[0]
     green_ratio = color2[1] / number2
@@ -81,24 +116,36 @@ def fade_in(color2, fade_time2, number2 = 50):
         np.show()
         time.sleep(fade_time2)
         
-def chase():
+def chase(col1, col2):
+    """
+    
+    Args:
+    
+    Return: none
+    """
     count = 0
     for i in range(np.n):
-        np.fill(orange)
+        np.fill(col1)
         for i in range(np.n):
             if (i + count) % 3 == 0:
-                np[i] = (black)
+                np[i] = (col2)
         time.sleep(0.2)
         np.show()
         count = (count + i) % 3
         
-def chase2():
+def chase2(col1, col2):
+    """
+    
+    Args:
+    
+    Return: none
+    """
     count = 0
     for i in range(np.n):
-        np.fill(green)
+        np.fill(col1)
         for i in range(np.n):
             if (i - count) % 3 == 0:
-                np[i] = (orange)
+                np[i] = (col2)
         time.sleep(0.2)
         np.show()
         count = (count + i) % 3
@@ -106,28 +153,19 @@ def chase2():
 while True:
     nice = random.choice(halloween_list)
     fade_in(purple, .02)
-    for i in range(5):
-        lightning(orange, purple)
-    chase()
-    fade_out(orange, .02)
-    chase2()
+    chase(orange, black)
+    chase2(purple, black)
     fade_out(purple, .02)
+    fade_in(green, .02)
+    for i in range(3):
+        lightning(purple, red)
+    sparkle(purple, red, .2, 5)
     fire()
+    fade_out(orange, .02)
     for i in range(3):
-        lightning(orange, purple)
-    fire()
-    fade_out(orange, .002)
-    fade_in(dark_blue, 0.02)
-    time.sleep(.2)
-    fade_in(dark_blue, 0.02)
-    time.sleep(.2)
-    fade_in(dark_blue, 0.02)
-    np.fill(dark_blue)
-    for i in range(3):
-        lightning(dark_blue, red)
-    time.sleep(.2)
-    for i in range(3):
-        lightning(dark_blue, red)
-    time.sleep(.2)
-    for i in range(3):
-        lightning(dark_blue, red)
+        lightning(black, red)
+    fade_in(red, .02)
+    sparkle(red, black, .2, 10)
+    for i in range(5):
+        lightning(red, white)
+    fade_out(red, .02)
